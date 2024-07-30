5d321172b2b6df67e16a71197631eb6090b6da2b
pranay1uv <168066729+pranay1uv@users.noreply.github.com>
2024-07-30 18:52:18 +0530

    Time: 0 ms (100%), Space: 41.2 MB (87.63%) - LeetHub

--- 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.java
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.java
@@ -1,10 +1,6 @@
 class Solution {
     public int maxDepth(String s) {
-        // Input is String which is valid parantheses string
-        // output is int
-        // I have to return the max number of nested parantheses in string
-        // At the edn of string traversal i can easily find the nesting depth of parantheses
-        int maxValue = Integer.MIN_VALUE;
+        int maxValue = 0;
         int depth = 0;
         for(int i = 0; i<s.length(); i++){
             if(s.charAt(i) == '('){
@@ -12,9 +8,6 @@ class Solution {
             }else if(s.charAt(i) == ')'){
                 maxValue = Math.max(maxValue,depth);
                 depth -= 1;
-            }else{
-                depth = depth;
-                maxValue = Math.max(maxValue,depth);
             }
         }
         return maxValue;
