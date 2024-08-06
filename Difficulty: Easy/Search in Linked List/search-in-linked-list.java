cdb2122222714d53252534833086dd442ea5f737
pranay1uv <168066729+pranay1uv@users.noreply.github.com>
2024-08-06 15:54:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in Linked List/search-in-linked-list.java
@@ -0,0 +1,67 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int x) {
+        data = x;
+        next = null;
+    }
+
+} class GFG {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t > 0) {
+            int n = sc.nextInt();
+            Node head = new Node(sc.nextInt());
+            Node tail = head;
+            for (int i = 0; i < n - 1; i++) {
+                tail.next = new Node(sc.nextInt());
+                tail = tail.next;
+            }
+            int key = sc.nextInt();
+            Solution g = new Solution();
+            boolean ans = g.searchKey(n, head, key);
+            System.out.println(ans ? "1" : "0");
+            // printList(head);
+            t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/* Node of a linked list
+  class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+class Solution {
+    static boolean searchKey(int n, Node head, int key) {
+        // Code here
+        // Input is Node Head, keySearch in Node, No of nodes
+        // Ouput is boolean true or false
+        // if the key is present in any of the nodes of the linkedlist
+        // i have to return true otherwise false
+        // Logic to be devised 
+        // i will intiate a boolean value to true;
+        
+        Node currentNode = head;
+        while(currentNode!=null){
+            if(currentNode.data == key){
+                return true;
+            }
+            currentNode = currentNode.next;
+        }return false;
+    }
+}
