bb60cdfbd99eca777ec8807ef776f85274d79dfc
pranay1uv <168066729+pranay1uv@users.noreply.github.com>
2024-08-07 15:37:06 +0530

    Time: 0 ms (100%), Space: 41.2 MB (26.05%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
@@ -10,32 +10,11 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
-        // Input is Node - headNode
-        // Output is Node
-        // i have to return the middle node of a linkedlist
-        // if No of nodes is even , middle node will be one , so no prob
-        // if it is odd , there will be two middle nodes 
-        // in that case i have to return second middle node
-        // Logic to be devised
-        // first thing is i have to calculate the no of nodes in the LL
-        // from the examples i have found that if i divide the No of Nodes
-        // Then quotient + 1 will be middle Node either it is od or evn
-
-        ListNode temp = head;
-        int count = 0;
-        for(int i = 0; temp!=null;i++){
-            count++;
-            temp = temp.next;
-        }
-        int middleNode = count/2 + 1;
-        ListNode tempr = head;
-        for(int i = 1; i<=middleNode; i++){
-            if(i==middleNode){
-                return tempr;
-            }
-            tempr = tempr.next;
-
-
-        }return tempr;
+        ListNode full = head;
+        ListNode half = head;
+        while(full!=null && full.next!=null){
+            full = full.next.next;
+            half = half.next;
+        }return half;
     }
 }
